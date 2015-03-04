6d49c5e5868eaac7d551a2e7819671e4f9a7fb50
wubang <wolfwubang@gmail.com>
2015-03-04 21:56:34 +0800

    add the see_message feature

---
+++ features/step_definitions/see_messages.rb
@@ -0,0 +1,15 @@
+Given(/^there is a User$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the User has posted the message "(.*?)"$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I visit the page for the User$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should see "(.*?)"$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
